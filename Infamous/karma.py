9d5e2601f216e57fcbaa42dbcce809094ddb8339
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-09-15 03:21:58 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -10,26 +10,26 @@ from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
 # <============================================== CONSTANTS =========================================================>
 START_IMG = [
-    "https://telegra.ph/file/40b93b46642124605e678.jpg",
-    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
-    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
-    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
-    "https://telegra.ph/file/262c86393730a609cdade.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "https://graph.org/file/df910c1a7c251c298f4b5.jpg",
+    "https://graph.org/file/df910c1a7c251c298f4b5.jpg",
+    "https://telegra.ph/file/50da5116b6d31ae9ca840.jpg",
+    "https://telegra.ph/file/50da5116b6d31ae9ca840.jpg",
+    "https://graph.org/file/df910c1a7c251c298f4b5.jpg",
+    "https://graph.org/file/a89a03efc7f4bf077a4a6.jpg",
+    "https://graph.org/file/df910c1a7c251c298f4b5.jpg",
 ]
 
-HEY_IMG = "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg"
+HEY_IMG = "https://graph.org/file/a89a03efc7f4bf077a4a6.jpg"
 
 ALIVE_ANIMATION = [
-    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
-    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
-    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
-    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+    "https://graph.org/file/a89a03efc7f4bf077a4a6.jpg",
+    "https://graph.org/file/df910c1a7c251c298f4b5.jpg",
+    "https://telegra.ph/file/50da5116b6d31ae9ca840.jpg",
+    "https://telegra.ph/file/50da5116b6d31ae9ca840.jpg",
+    "https://graph.org/file/a89a03efc7f4bf077a4a6.jpg",
+    "https://telegra.ph/file/265a628a568d7a902057a.jpg",
+    "https://telegra.ph/file/2829138cd6da27fcae483.jpg",
+    "https://telegra.ph/file/265a628a568d7a902057a.jpg",
 ]
 
 FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
@@ -71,8 +71,8 @@ GROUP_START_BTN = [
 
 ALIVE_BTN = [
     [
-        ib(text="UPDATES", url="https://t.me/Hydra_Updates"),
-        ib(text="SUPPORT", url="https://t.me/hydraXsupport"),
+        ib(text="UPDATES", url="https://t.me/about_tosuu"),
+        ib(text="SUPPORT", url="https://t.me/nothing_bots_support"),
     ],
     [
         ib(
